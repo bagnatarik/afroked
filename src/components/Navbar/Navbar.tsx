211d29cd8fdea93206d1b2d36f90a62b57c9de71
Tarik BAGNA <tarikbagnapro@gmail.com>
2024-03-24 00:27:32 +0100

    changes

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,7 @@
-import NavbarMenu from "../NavbarMenu/NavbarMenu";
-
 export default function Navbar() {
     return (
         <>
-            <nav className="flex items-center px-5 py-7 w-full">
-                <p className='uppercase font-extrabold text-xl text-white grow'>afroked</p>
-                <NavbarMenu className="flex justify-between items-center uppercase" />
-            </nav>
+
         </>
     )
 }
