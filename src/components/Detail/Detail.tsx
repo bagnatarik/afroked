1aa9715b5876991795bc1d051ab7cdc52bd70d5e
Tarik BAGNA <tarikbagnapro@gmail.com>
2024-03-28 11:56:14 +0100

    changes : updates v5

--- src/components/Detail/Detail.tsx
+++ src/components/Detail/Detail.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
-import { Button } from "../Section/Section";
+import { Button, Product } from "../Section/Section";
 
 /* export async function loader({ params }) {
     return params.productId;
@@ -9,7 +9,7 @@ import { Button } from "../Section/Section";
 
 export async function loader({ params }) {
     const fetch = async () => {
-        const response = await axios.get(`https://afroked.onrender.com/api/products/${params.productId}`)
+        const response = await axios.get<Product>(`https://afroked.onrender.com/api/products/${params.productId}`)
         return response.data ?? null
     }
 
@@ -18,7 +18,7 @@ export async function loader({ params }) {
 
 export default function Detail() {
 
-    const [data, setData] = useState(useLoaderData())
+    const [data, setData] = useState<Product>(useLoaderData())
     const [count, setCount] = useState(0)
 
     const plus = () => {
@@ -37,11 +37,10 @@ export default function Detail() {
             <div className="grow flex flex-col items-center w-full h-full">
                 <img className="w-full h-[450px] mb-3" src={data!.image} alt="" />
                 <div className="px-3">
-                    <p className="font-bold text-2xl mb-2 uppercase">{data!.name}</p>
-                    <p className="font-normal text-sm mb-2">{data!.description}</p>
+                    <p className="font-bold text-2xl mb-2 capitalize">{data!.name}</p>
 
                     <div className="flex items-center">
-                        <p className="font-bold text-2xl mb-2 uppercase w-1/2">{data!.price} CFA</p>
+                        <p className=" text-2xl mb-2 uppercase w-1/2">{data!.price} CFA</p>
                         <div className="w-1/2 h-16 bg-gray-200 border-gray-400 rounded-lg flex items-center justify-between px-2">
                             <svg onClick={minus} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M5 12h14" />
@@ -55,7 +54,7 @@ export default function Detail() {
                         </div>
                     </div>
                     <Button id={undefined} onclick={() => { }} className="text-sm w-full flex-shrink-0 items-center justify-center my-4 hover:bg-gray-700 flex py-5 px-10 mr-2 bg-gray-800 text-white rounded-lg" text="Ajouter" isSearchButton={false} />
-
+                    <p className="font-normal text-sm mb-2">{data!.description}</p>
                 </div>
             </div>
         </>
